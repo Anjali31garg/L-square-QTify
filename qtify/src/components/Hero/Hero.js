f78779f728f3fd228d4449509db97908a2a0d9ee
Anjali Garg <anjaligarg@dentalkart.com>
2024-03-19 22:48:10 +0530

    testing hero

--- qtify/src/components/Hero/Hero.js
+++ qtify/src/components/Hero/Hero.js
@@ -1,22 +1,28 @@
 import React from "react";
-import styles from './Hero.module.css';
-import hero from '../../assests/head.png';
-const Hero = () =>{
-    return(
-        <>
-
-        <section className={styles.Hero}>
-        <div className={styles.HeroTitle}>
+import styles from "./Hero.module.css";
+import hero from "../../assests/head.png";
+const Hero = () => {
+  return (
+    <>
+      {/* <div className={styles.HeroTitle}>
             <h1>100 Thousands Songs, ad-free </h1>
             <h1>Over thousands podcast episodes</h1>
            
-        </div>
+        
 
         <img className={styles.HeroImg} src={hero} alt="headphone"/>
-        
-        </section>
-        </>
-    )
-}
+         */}
+
+      <div className={styles.heroSection}>
+        <h1>
+          100 Thousand Songs, ad-free <br />
+          Over thousands podcast episodes
+        </h1>
+
+        <img src={hero} alt="headphone" />
+      </div>
+    </>
+  );
+};
 
 export default Hero;
