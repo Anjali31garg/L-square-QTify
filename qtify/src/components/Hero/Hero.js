c0135a8227e51e532be76730013ca9bea4fbdf1c
Anjali Garg <anjaligarg@dentalkart.com>
2024-03-19 17:31:12 +0530

    hero section completed

--- qtify/src/components/Hero/Hero.js
+++ qtify/src/components/Hero/Hero.js
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from './Hero.module.css';
-import hero from '../../assests/hero.png';
+import hero from '../../assests/head.png';
 const Hero = () =>{
     return(
         <>
-        
+
         <div className={styles.heroSection}>
+        
+            <h1>100 Thousands Songs, ad-free <br/> Over thousands podcast episodes</h1>
+           
+        
         <img src={hero}/>
+        
         </div>
         </>
     )
